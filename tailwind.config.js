421d33f9feb7871781fa862248b62d9c25713a3f
Wyatt Rajer <wyattrajer@gmail.com>
2023-11-15 14:16:36 -0500

    creates 2 fonts, prata and patua

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'palm-leaf': '#f2fbea',
-      }
+        "palm-leaf": "#f2fbea",
+        "wheat-field": "#3D1B0B",
+      },
+      fontFamily: {
+        patua: ["'Patua One', serif"],
+        prata: ["'Prata', serif"]
+      },
     },
   },
   plugins: [],
-}
-
+};
