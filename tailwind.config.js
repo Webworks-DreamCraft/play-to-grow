d4ea08dbec8049726453dce4b56d86116c2514b0
Wyatt Rajer <wyattrajer@gmail.com>
2023-11-15 13:45:58 -0500

    creates 2 colors palm-leaf for green and wheatfield for brown

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'palm-leaf': '#f2fbea',
+      }
+    },
   },
   plugins: [],
 }
