521bc1897c3fc7484d1ad4b4279ae87107bc8602
Wyatt Rajer <wyattrajer@gmail.com>
2023-11-27 16:35:20 -0500

    stashing

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
     <>
       <section className="md:container mx-auto bg-palm-leaf">
         <p className="text-wheat-field">Play to Grow</p>
-        <Footer />
       </section>
+        <Footer />
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,22 @@ import Logo from "./reusables/Logo"
 
 const Footer = () => {
   return (
-    <section className="flex flex-col justify-center font-prata">
-      <section className="flex justify-center">
+    <section className="flex flex-col font-prata bg-palm-leaf">
+      <section className="flex justify-center mt-8">
         <Logo width="232" height="238"/>
       </section>
-      <section className="flex justify-center my-8 text-lg">
-        <nav className="flex justify-between w-9/12">
+      <section className="flex justify-center py-8 text-lg">
+        <nav className="flex justify-between w-11/12 xxs:w-9/12 sm:w-[276px]">
+          {/* These need to be updated to Links from react-router-dom when implemented */}
           <a>Home</a>
           <a>Services</a>
           <a>About Us</a>
         </nav>
       </section>
-      <section className="flex justify-center mt-8">
+      <section className="w-full flex justify-center">
+        <section className="flex justify-center w-11/12 xs:w-10/12 border-b-[1px] border-solid border-wheat-field-dark"></section>
+      </section>
+      <section className="flex justify-center pb-4 pt-8">
         <p className="text-sm">© 2023 Play To Grow. All rights reserved.</p>
       </section>
     </section>
