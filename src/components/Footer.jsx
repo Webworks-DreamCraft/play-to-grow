5518aca06c2908a1a6995b87293daa9f73c3ffc3
Wyatt Rajer <wyattrajer@gmail.com>
2023-12-04 14:58:59 -0500

    changes Logo from component to png, fixes double hidden bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "./reusables/Logo"
 const Footer = () => (
     <section className="flex flex-col font-prata bg-palm-leaf">
       <section className="flex justify-center mt-8">
-        <Logo width="232" height="238"/>
+        <img src="../public/logo.png" width="232" height="238"/>
       </section>
       <section className="flex justify-center py-8 text-lg">
         <nav className="flex justify-between w-11/12 xxs:w-9/12 sm:w-[276px]">
--- src/components/IntakeForm.jsx
+++ src/components/IntakeForm.jsx
@@ -92,9 +92,8 @@ const IntakeForm = () => {
           </section>
         </section>
         <section className="hidden md:flex pt-56">
-          <section>
-            <Logo width="421" height="431" />
-          </section>
+            {/* <Logo width="421" height="431" /> */}
+            <img src="../public/logo.png" width="421" height="431"/>
         </section>
       </section>
       <section className="flex justify-center w-full bg-white border-[0.5px] md:w-[1377px] md:mt-16 rounded-[10px]">
