950eb50a3712dc56cc574bf88a238de2113ce79b
Wyatt Rajer <wyattrajer@gmail.com>
2023-12-04 15:20:44 -0500

    update image path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => (
     <section className="flex flex-col font-prata bg-palm-leaf">
       <section className="flex justify-center mt-8">
-        <img src="../public/logo.png" width="232" height="238"/>
+        <img src="logo.png" width="232" height="238"/>
       </section>
       <section className="flex justify-center py-8 text-lg">
         <nav className="flex justify-between w-11/12 xxs:w-9/12 sm:w-[276px]">
