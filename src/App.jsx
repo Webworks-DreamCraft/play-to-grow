1b0b4694c0da53a1ee8da0a98a48f74c731693e6
babywipez <cassada.chris@gmail.com>
2023-12-07 13:21:42 -0500

    add routes and create shell components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import Footer from "./components/Footer"
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import About from "./components/pages/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-
   return (
     <>
-      <section className="md:container mx-auto bg-palm-leaf">
-        <p className="text-wheat-field">Play to Grow</p>
-      </section>
-        <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,25 @@
-import Logo from "./reusables/Logo"
+import Logo from "./reusables/Logo";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
-    <section className="flex flex-col font-prata bg-palm-leaf">
-      <section className="flex justify-center mt-8">
-        <Logo width="232" height="238"/>
-      </section>
-      <section className="flex justify-center py-8 text-lg">
-        <nav className="flex justify-between w-11/12 xxs:w-9/12 sm:w-[276px]">
-          {/* These need to be updated to Links from react-router-dom when implemented */}
-          <a>Home</a>
-          <a>Services</a>
-          <a>About Us</a>
-        </nav>
-      </section>
-      <section className="w-full flex justify-center">
-        <section className="flex justify-center w-11/12 xs:w-10/12 border-b-[1px] border-solid border-wheat-field-dark"></section>
-      </section>
-      <section className="flex justify-center pb-4 pt-8">
-        <p className="text-sm">© 2023 Play To Grow. All rights reserved.</p>
-      </section>
+  <section className="flex flex-col font-prata bg-palm-leaf">
+    <section className="flex justify-center mt-8">
+      <Logo width="232" height="238" />
     </section>
-  )
+    <section className="flex justify-center py-8 text-lg">
+      <nav className="flex justify-around w-11/12 xxs:w-9/12 sm:w-[276px]">
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/about">About Us</Link>
+      </nav>
+    </section>
+    <section className="w-full flex justify-center">
+      <section className="flex justify-center w-11/12 xs:w-10/12 border-b-[1px] border-solid border-wheat-field-dark"></section>
+    </section>
+    <section className="flex justify-center pb-4 pt-8">
+      <p className="text-sm">© 2023 Play To Grow. All rights reserved.</p>
+    </section>
+  </section>
+);
 
-export default Footer
+export default Footer;
