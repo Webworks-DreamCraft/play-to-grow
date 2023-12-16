c7781541bf12b5cb0fa5635201db8856dff0f2bf
babywipez <cassada.chris@gmail.com>
2023-12-16 17:52:51 -0500

    adds navbar, navmodal, and related packages

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,18 @@
+// This is just test code to test navigation
+
 const Home = () => {
   return (
     <section>
       <h1>Home Page</h1>
+      <section>
+        <section className="bg-yellow-100 h-[1100px]"></section>
+        <h4 id="form">Form Goes Here</h4>
+        <ul>
+          <li>Name</li>
+          <li>Kid's name</li>
+          <li>Etc.</li>
+        </ul>
+      </section>
     </section>
   );
 };
