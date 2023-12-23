6f26f0a5aadf6972559a22f0214a651acf9740b0
Wyatt Rajer <wyattrajer@gmail.com>
2023-12-23 11:46:04 -0500

    confirmation styling

--- src/components/pages/Confirmation.jsx
+++ src/components/pages/Confirmation.jsx
@@ -1,6 +1,15 @@
 const Confirmation = () => {
   return(
-    <h1>Confirmation</h1>
+    <section className="text-wheat-field-dark text-center md:container mx-auto">
+      <p className="font-patua text-4xl md:text-[120px] py-10">Thank You!</p>
+      <p className="font-prata text-base md:text-2xl pb-8 md:pb-20">I have received your documentation and will get back to you within three to five business days to discuss additional details. I look forward to speaking with you!</p>
+      <button
+          type="submit"
+          className="font-prata text-lg bg-wheat-field-middle md:mb-12 py-3 px-8 rounded-[10px]"
+        >
+          Home
+        </button>
+    </section>
   )
 }
 
