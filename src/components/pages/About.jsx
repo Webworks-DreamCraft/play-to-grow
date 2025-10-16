4da9518edc9ef2e348517918ba39330f8ab19326
lukepate <lpate@spotify.com>
2025-10-15 20:13:24 -0400

    fix padding and names

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -12,44 +12,54 @@ const About = () => (
         </p>
       </section>
     </section>
-    <section className="lg:flex lg:items-center lg:text-left px-8 md:px-16 text-center pt-14 xl:pb-24 md:pt-16">
-      <section className="">
+    <section className="text-wheat-field-dark md:container mx-auto">
+      <section className="text-center pt-14">
         <h2 className="font-patua text-[34px] md:text-6xl">
           Meet Your Care Providers
         </h2>
-        <p className="font-prata text-lg py-6 lg:pr-28">
-          Hello! I'm Katie Gonzalez, the owner of Play to Grow and practicing
-          Speech-Language Pathologist. I'm incredibly passionate about making a
-          meaningful difference in the lives of children with communication
-          challenges. My educational journey began at the University of Florida,
-          where I earned my undergraduate degree in Communication Sciences and
-          Disorders. Building on this foundation, I pursued my Master of Science
-          degree at the University of South Florida, further honing my skills
-          and knowledge in the field. It was during this time that I developed a
-          deep appreciation for the complexity of communication and the profound
-          impact it has on individuals' lives.
-        </p>
       </section>
-      <section className="flex justify-center lg:pl-2">
-        <img className="rounded-lg max-w-[480px]" src="/katie.jpeg" />
+      <section className="lg:flex lg:items-center lg:text-left px-8 md:px-16 text-center pt-14 xl:pb-24 md:pt-16">
+        <section className="">
+          <h3 className="font-patua text-[28px] md:text-[36px] pt-8 md:pt-0">
+            Katie Gonzalez
+          </h3>
+          <p className="font-prata text-lg py-6 lg:pr-28">
+            Hello! I&apos;m Katie Gonzalez, the owner of Play to Grow and
+            practicing Speech-Language Pathologist. I&apos;m incredibly
+            passionate about making a meaningful difference in the lives of
+            children with communication challenges. My educational journey began
+            at the University of Florida, where I earned my undergraduate degree
+            in Communication Sciences and Disorders. Building on this
+            foundation, I pursued my Master of Science degree at the University
+            of South Florida, further honing my skills and knowledge in the
+            field. It was during this time that I developed a deep appreciation
+            for the complexity of communication and the profound impact it has
+            on individuals&rsquo; lives.
+          </p>
+        </section>
+        <section className="flex justify-center lg:pl-2">
+          <img className="rounded-lg max-w-[480px]" src="/katie.jpeg" />
+        </section>
       </section>
-    </section>
-    <section className="text-wheat-field-dark md:container mx-auto">
       <section className="lg:flex lg:items-center lg:text-left px-8 md:px-16 text-center pt-4 xl:pb-24">
         <section className="">
+          <h3 className="font-patua text-[28px] md:text-[36px] pt-8 md:pt-0">
+            Erin Bradley
+          </h3>
           <p className="font-prata text-lg py-6 lg:pr-28">
-            I'm Erin! I was raised in Gainesville, received my undergraduate
-            degree from the University of Florida, and my master’s degree from
-            Florida State University. I have worked in a variety of settings,
-            including schools and outpatient clinics, and have extensive
-            experience supporting children with diverse communication profiles,
-            including those with complex communication needs. With specialized
-            training in AAC and gestalt language processing, my approach is
-            rooted in compassionate collaboration, total communication, and
-            neurodiversity-affirming practices. I am passionate about empowering
-            caregivers to build confidence in supporting their child’s
-            communication growth, celebrating each child’s unique strengths, and
-            helping children express themselves in meaningful ways.
+            I&apos;m Erin! I was raised in Gainesville, received my
+            undergraduate degree from the University of Florida, and my master’s
+            degree from Florida State University. I have worked in a variety of
+            settings, including schools and outpatient clinics, and have
+            extensive experience supporting children with diverse communication
+            profiles, including those with complex communication needs. With
+            specialized training in AAC and gestalt language processing, my
+            approach is rooted in compassionate collaboration, total
+            communication, and neurodiversity-affirming practices. I am
+            passionate about empowering caregivers to build confidence in
+            supporting their child’s communication growth, celebrating each
+            child’s unique strengths, and helping children express themselves in
+            meaningful ways.
           </p>
         </section>
         <section className="flex justify-center lg:pl-2">
@@ -58,6 +68,9 @@ const About = () => (
       </section>
       <section className="lg:flex lg:items-center lg:text-left px-8 md:px-16 text-center pt-4 xl:pb-24">
         <section className="">
+          <h3 className="font-patua text-[28px] md:text-[36px] pt-8 md:pt-0">
+            Sabrina Roque
+          </h3>
           <p className="font-prata text-lg py-6 lg:pr-28">
             Hi! I’m Sabrina, one of the Speech-Language Pathologists at Play to
             Grow.
@@ -77,15 +90,18 @@ const About = () => (
       </section>
       <section className="lg:flex lg:items-center lg:text-left px-8 md:px-16 text-center pt-4 xl:pb-24">
         <section className="">
+          <h3 className="font-patua text-[28px] md:text-[36px] pt-8 md:pt-0">
+            Amy Bouchard
+          </h3>
           <p className="font-prata text-lg py-6 lg:pr-28">
-            Hey! I’m Amy Bouchard, I’m a Speech Language Pathology Assistant
-            (SLPA) here at Play to Grow. I graduated from the University of
-            Florida (go gators!) with my bachelor’s in Communication Sciences
-            and Disorders. I am passionate about and have experience working
-            with high-tech AAC, kids with ASD, and the Natural Language
-            Acquisition (NLA/GLP) frame-work. One of my priorities is
-            incorporating neurodiversity-affirming practices, and I am always
-            seeking to learn more. I look forward to growing with you!
+            Hey! I’m Amy, I’m a Speech Language Pathology Assistant (SLPA) here
+            at Play to Grow. I graduated from the University of Florida (go
+            gators!) with my bachelor’s in Communication Sciences and Disorders.
+            I am passionate about and have experience working with high-tech
+            AAC, kids with ASD, and the Natural Language Acquisition (NLA/GLP)
+            frame-work. One of my priorities is incorporating
+            neurodiversity-affirming practices, and I am always seeking to learn
+            more. I look forward to growing with you!
           </p>
         </section>
         <section className="flex justify-center lg:pl-2">
@@ -94,7 +110,7 @@ const About = () => (
       </section>
       <section className="flex flex-col justify-center text-center px-7 pb-8 md:py-28">
         <h2 className="py-8 font-patua text-[34px] md:text-[56px]">
-          Let’s Start Working Together !
+          Let’s Start Working Together!
         </h2>
         <section className="mx-auto">
           <HashLink to="/#form">
